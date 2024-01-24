d0232df71055e48d16796fdd393f51dc390ccd31
AVAniketh0905 <adimoolamaniketh@gmail.com>
2024-01-24 17:45:23 +0530

    refactor animation class

--- fluidspylib/fluidspy/animate/animation.py
+++ fluidspylib/fluidspy/animate/animation.py
@@ -5,61 +5,73 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.widgets import Slider
 
 
-class SimulationAnimation:
-    def __init__(self, states: List[List], repeat: bool = False):
+class SliderHandler:
+    def __init__(self, ax, num_frames):
+        self.slider = Slider(ax, "Time Step", 0, num_frames - 1, valinit=0, valstep=1)
+
+    def on_changed(self, callback):
+        self.slider.on_changed(callback)
+
+    def set_value(self, value):
+        self.slider.set_val(value)
+
+
+class PlottingHandler:
+    def __init__(self, states):
         self.states = states
         self.num_frames = len(states)
 
-        # Create a figure without axes
         self.fig, self.axes = plt.subplots()
-        self.fig.subplots_adjust(bottom=0.2)  # Adjust the bottom margin for the slider
+        self.fig.subplots_adjust(bottom=0.2)
 
-        # Create an initial plot (assuming it's a 2D plot)
         self.image = plt.imshow(states[0], cmap="viridis")
 
-        # Center the slider
-        slider_width = 0.65
-        slider_height = 0.03
-        slider_x = (1 - slider_width) / 2  # Center the slider horizontally
-        slider_y = 0.01  # Adjust the vertical position if needed
-        self.ax_slider = plt.axes([slider_x, slider_y, slider_width, slider_height])
-
-        self.slider = Slider(
-            self.ax_slider, "Time Step", 0, self.num_frames - 1, valinit=0, valstep=1
-        )
-
-        # Add a legend for the color range
         self.fig.colorbar(self.image, orientation="vertical", label="Color Range")
-
-        # Hide the axes completely
         self.axes.axes.get_xaxis().set_visible(False)
         self.axes.axes.get_yaxis().set_visible(False)
 
-        # Set up the animation with repeat parameter
-        self.ani = FuncAnimation(
-            self.fig, self.update, frames=self.num_frames, blit=True, repeat=repeat
-        )
-
-        # Connect the slider to the update function
-        self.slider.on_changed(self.update_slider)
+    def update_plot(self, frame):
+        current_state = self.states[frame]
+        self.image.set_array(current_state)
 
     def set_title(self, title):
-        self.fig.suptitle(title)
+        self.axes.set_title(title)
+
+    def show_plot(self):
+        plt.show()
+
+
+class UpdateHandler:
+    def __init__(self, plotting_handler, slider_handler):
+        self.plotting_handler = plotting_handler
+        self.slider_handler = slider_handler
 
     def update(self, frame):
-        current_state = self.states[frame]
-        self.image.set_array(current_state)
+        self.plotting_handler.update_plot(frame)
+        self.slider_handler.set_value(frame)
+        return [self.plotting_handler.image]
 
-        # Set the slider value to the current frame
-        self.slider.set_val(frame)
 
-        return [self.image]
+class SimulationAnimation:
+    def __init__(self, states: List[List], repeat: bool = False):
+        self.plotting_handler = PlottingHandler(states)
 
-    def update_slider(self, val):
-        frame = int(val)
-        current_state = self.states[frame]
-        self.image.set_array(current_state)
-        plt.draw()
+        ax_slider = self.plotting_handler.fig.add_axes([0.2, 0.01, 0.65, 0.03])
+        self.slider_handler = SliderHandler(ax_slider, len(states))
+        self.update_handler = UpdateHandler(self.plotting_handler, self.slider_handler)
+
+        self.ani = FuncAnimation(
+            self.plotting_handler.fig,
+            self.update_handler.update,
+            frames=len(states),
+            blit=True,
+            repeat=repeat,
+        )
+
+        self.slider_handler.on_changed(self.update_handler.update)
+
+    def set_title(self, title):
+        self.plotting_handler.set_title(title)
 
     def show_animation(self):
-        plt.show()
+        self.plotting_handler.show_plot()
